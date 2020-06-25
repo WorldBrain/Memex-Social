841cbefb1df9faef779682299a74f448d2565449
Vincent den Boer <vincent@shishkabab.net>
2020-06-25 17:40:41 +0200

    Add topological sorting during bootstrap

---
+++ tools/ordered-packages.ts
@@ -0,0 +1,35 @@
+import * as _ from "lodash"
+import * as fs from "fs"
+import * as path from "path"
+import * as glob from "glob"
+const toposort = require('toposort')
+
+export async function main() {
+    const workspaceDir = path.resolve(__dirname, '..')
+    const lernaConfig = JSON.parse(fs.readFileSync(path.join(workspaceDir, 'lerna.json')).toString())
+    const packageGlobs: string[] = lernaConfig['packages']
+    const potentialPackageDirs = _.flatten(packageGlobs
+        .map(packageGlob => glob.sync(path.join(workspaceDir, packageGlob))))
+    const packageDirs = potentialPackageDirs.filter(dir => fs.existsSync(path.join(dir, 'package.json')))
+    const packageInfoList = packageDirs.map(dir => JSON.parse(fs.readFileSync(path.join(dir, 'package.json')).toString()))
+    const packageInfoByName = _.fromPairs(packageInfoList.map(info => [info.name, info]))
+
+    const packageNames = Object.keys(packageInfoByName)
+    const packageEdges: Array<[string, string]> = []
+    for (const packageInfo of packageInfoList) {
+        for (const dependencyList of [packageInfo.dependencies, packageInfo.devDependencies ?? []]) {
+            for (const dependency of Object.keys(dependencyList)) {
+                if (packageInfoByName[dependency]) {
+                    packageEdges.push([packageInfo.name, dependency])
+                }
+            }
+        }
+    }
+
+    const sorted = toposort.array(packageNames, packageEdges).reverse()
+    console.log(sorted.join(' '))
+}
+
+if (require.main === module) {
+    main()
+}
