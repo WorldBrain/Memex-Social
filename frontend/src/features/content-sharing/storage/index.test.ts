7002172f85d6dddb708b977586cf5fe9805c4ca6
Vincent den Boer <vincent@shishkabab.net>
2020-07-30 12:57:46 +0200

    Clean up content sharing storage tests

--- frontend/src/features/content-sharing/storage/index.test.ts
+++ frontend/src/features/content-sharing/storage/index.test.ts
@@ -1,6 +1,65 @@
 import sortBy from 'lodash/sortBy'
 import expect from 'expect'
+import { SharedListReference } from '@worldbrain/memex-common/lib/content-sharing/types'
+import { UserReference } from '@worldbrain/memex-common/lib/web-interface/types/users'
 import { createStorageTestSuite } from '../../../tests/storage-tests'
+import ContentSharingStorage from '.'
+
+async function createTestListEntries(params: {
+    contentSharing: ContentSharingStorage, listReference: SharedListReference, userReference: UserReference
+}) {
+    await params.contentSharing.createListEntries({
+        listReference: params.listReference,
+        listEntries: [
+            {
+                entryTitle: 'Page 1',
+                originalUrl: 'https://www.foo.com/page-1',
+                normalizedUrl: 'foo.com/page-1',
+            },
+            {
+                entryTitle: 'Page 2',
+                originalUrl: 'https://www.bar.com/page-2',
+                normalizedUrl: 'bar.com/page-2',
+                createdWhen: 592,
+            },
+        ],
+        userReference: params.userReference
+    })
+}
+
+async function createTestAnnotations(params: {
+    contentSharing: ContentSharingStorage, listReference: SharedListReference, userReference: UserReference
+}) {
+    await params.contentSharing.createAnnotations({
+        listReferences: [params.listReference],
+        creator: params.userReference,
+        annotationsByPage: {
+            'foo.com/page-1': [
+                {
+                    createdWhen: 500,
+                    body: 'Body 1',
+                    comment: 'Comment 1',
+                    selector: 'Selector 1',
+                },
+                {
+                    createdWhen: 1500,
+                    body: 'Body 2',
+                    comment: 'Comment 2',
+                    selector: 'Selector 2',
+                },
+            ],
+            'bar.com/page-2': [
+                {
+                    createdWhen: 2000,
+                    body: 'Body 3',
+                    comment: 'Comment 3',
+                    selector: 'Selector 3',
+                },
+            ],
+        }
+    })
+}
+
 
 createStorageTestSuite('Content sharing storage', ({ it }) => {
     it('should save lists and retrieve them', { withTestUser: true }, async ({ storage, services }) => {
@@ -70,22 +129,8 @@ createStorageTestSuite('Content sharing storage', ({ it }) => {
             localListId: 55,
             userReference
         })
-        await contentSharing.createListEntries({
-            listReference,
-            listEntries: [
-                {
-                    entryTitle: 'Page 1',
-                    originalUrl: 'https://www.foo.com/page-1',
-                    normalizedUrl: 'foo.com/page-1',
-                },
-                {
-                    entryTitle: 'Page 2',
-                    originalUrl: 'https://www.bar.com/page-2',
-                    normalizedUrl: 'bar.com/page-2',
-                    createdWhen: 592,
-                },
-            ],
-            userReference
+        await createTestListEntries({
+            contentSharing, listReference, userReference
         })
         const retrieved = (await contentSharing.retrieveList(listReference))!
         retrieved.entries = sortBy(retrieved.entries, 'entryTitle')
@@ -133,22 +178,8 @@ createStorageTestSuite('Content sharing storage', ({ it }) => {
             localListId: 55,
             userReference
         })
-        await contentSharing.createListEntries({
-            listReference,
-            listEntries: [
-                {
-                    entryTitle: 'Page 1',
-                    originalUrl: 'https://www.foo.com/page-1',
-                    normalizedUrl: 'foo.com/page-1',
-                },
-                {
-                    entryTitle: 'Page 2',
-                    originalUrl: 'https://www.bar.com/page-2',
-                    normalizedUrl: 'bar.com/page-2',
-                    createdWhen: 592,
-                },
-            ],
-            userReference
+        await createTestListEntries({
+            contentSharing, listReference, userReference
         })
         await contentSharing.removeListEntries({
             listReference,
@@ -180,51 +211,8 @@ createStorageTestSuite('Content sharing storage', ({ it }) => {
             localListId: 55,
             userReference
         })
-        await contentSharing.createListEntries({
-            listReference,
-            listEntries: [
-                {
-                    entryTitle: 'Page 1',
-                    originalUrl: 'https://www.foo.com/page-1',
-                    normalizedUrl: 'foo.com/page-1',
-                },
-                {
-                    entryTitle: 'Page 2',
-                    originalUrl: 'https://www.bar.com/page-2',
-                    normalizedUrl: 'bar.com/page-2',
-                    createdWhen: 592,
-                },
-            ],
-            userReference
-        })
-        await contentSharing.createAnnotations({
-            listReferences: [listReference],
-            creator: userReference,
-            annotationsByPage: {
-                'foo.com/page-1': [
-                    {
-                        createdWhen: 500,
-                        body: 'Body 1',
-                        comment: 'Comment 1',
-                        selector: 'Selector 1',
-                    },
-                    {
-                        createdWhen: 1500,
-                        body: 'Body 2',
-                        comment: 'Comment 2',
-                        selector: 'Selector 2',
-                    },
-                ],
-                'bar.com/page-2': [
-                    {
-                        createdWhen: 2000,
-                        body: 'Body 3',
-                        comment: 'Comment 3',
-                        selector: 'Selector 3',
-                    },
-                ],
-            }
-        })
+        await createTestListEntries({ contentSharing, listReference, userReference })
+        await createTestAnnotations({ contentSharing, listReference, userReference })
         expect(await contentSharing.getAnnotationsForPagesInList({
             listReference,
             normalizedPageUrls: ['foo.com/page-1', 'bar.com/page-2']
