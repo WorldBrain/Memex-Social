1f0f6fdea625d31bafc4acb7970682b0438d3729
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2020-12-01 12:19:47 +0700

    Explicitly type theme

--- frontend/src/main-ui/styles/theme.ts
+++ frontend/src/main-ui/styles/theme.ts
@@ -1,43 +1,46 @@
 import { SpacingValue } from "styled-components-spacing";
 
-const spacing: { [Key in SpacingValue]: string } = {
-    none: '0',
-    smallest: '0.25rem',
-    small: '0.5rem',
-    medium: '1rem',
-    large: '1.5rem',
-    largest: '3rem',
-}
+import { Theme } from "./types";
 
-export const theme = {
-    spacing,
-    colors: {
-        background: 'white',
-        warning: 'red',
-        primary: '#3a2f45',
-        subText: '#aeaeae',
-        secondary: '#5cd9a6',
-        grey: '#e0e0e0',
-        black: '000',
-        overlay: {
-            background: 'rgba(0, 0, 0, 0.1)',
-            dialog: 'white',
-        }
+const spacing: { [Key in SpacingValue]: string } = {
+  none: "0",
+  smallest: "0.25rem",
+  small: "0.5rem",
+  medium: "1rem",
+  large: "1.5rem",
+  largest: "3rem",
+};
+
+export const theme: Theme = {
+  spacing,
+  colors: {
+    background: "white",
+    warning: "red",
+    primary: "#3a2f45",
+    subText: "#aeaeae",
+    secondary: "#5cd9a6",
+    grey: "#e0e0e0",
+    black: "000",
+    overlay: {
+      background: "rgba(0, 0, 0, 0.1)",
+      dialog: "white",
     },
-    fonts: {
-        primary: '"Poppins", sans-serif',
-    },
-    hoverBackground: {
-        primary: '#e0e0e0',
-    },
-    borderRadius: {
-        default: '3px',
-    },
-    fontSize: {
-        listTitle: '16px',
-        url: '14px',
-    },
-    zIndices: {
-        overlay: 50,
-    }
-}
+  },
+  fonts: {
+    primary: '"Poppins", sans-serif',
+    secondary: '"Poppins", sans-serif',
+  },
+  hoverBackground: {
+    primary: "#e0e0e0",
+  },
+  borderRadius: {
+    default: "3px",
+  },
+  fontSize: {
+    listTitle: "16px",
+    url: "14px",
+  },
+  zIndices: {
+    overlay: 50,
+  },
+};
--- frontend/src/main-ui/styles/types.ts
+++ frontend/src/main-ui/styles/types.ts
@@ -1,1 +1,28 @@
-export type ViewportBreakpoint = "mobile" | "small" | "big" | "normal"
+import { SpacingValue } from "styled-components-spacing";
+
+export type ViewportBreakpoint = "mobile" | "small" | "big" | "normal";
+
+export type ColorThemeKeys =
+  | "background"
+  | "warning"
+  | "primary"
+  | "secondary"
+  | "subText"
+  | "grey"
+  | "black";
+
+export type FontThemeKeys = "primary" | "secondary";
+
+export type FontSizeThemeKeys = "listTitle" | "url";
+
+export interface Theme {
+  spacing: { [Key in SpacingValue]: string };
+  colors: { [Key in ColorThemeKeys]: string } & {
+    overlay: { [Key in "background" | "dialog"]: string };
+  };
+  fonts: { [Key in FontThemeKeys]: string };
+  hoverBackground: { [Key in "primary"]: string };
+  fontSize: { [Key in FontSizeThemeKeys]: string };
+  borderRadius: { [Key in "default"]: string };
+  zIndices: { [Key in "overlay"]: number };
+}
