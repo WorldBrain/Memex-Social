33a7a1e193ef3c95e4d5699d1fa1f28a0dee52a7
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2020-12-01 12:43:19 +0700

    Move theme typings to memex common

--- frontend/src/main-ui/styles/types.ts
+++ frontend/src/main-ui/styles/types.ts
@@ -1,28 +1,3 @@
-import { SpacingValue } from 'styled-components-spacing'
+export { MemexTheme as Theme } from '@worldbrain/memex-common/lib/common-ui/styles/types'
 
 export type ViewportBreakpoint = 'mobile' | 'small' | 'big' | 'normal'
-
-export type ColorThemeKeys =
-  | 'background'
-  | 'warning'
-  | 'primary'
-  | 'secondary'
-  | 'subText'
-  | 'grey'
-  | 'black'
-
-export type FontThemeKeys = 'primary' | 'secondary'
-
-export type FontSizeThemeKeys = 'listTitle' | 'url'
-
-export interface Theme {
-  spacings: { [Key in SpacingValue]: string }
-  colors: { [Key in ColorThemeKeys]: string } & {
-    overlay: { [Key in 'background' | 'dialog']: string }
-  }
-  fonts: { [Key in FontThemeKeys]: string }
-  fontSizes: { [Key in FontSizeThemeKeys]: string }
-  hoverBackgrounds: { [Key in 'primary']: string }
-  borderRadii: { [Key in 'default']: string }
-  zIndices: { [Key in 'overlay']: number }
-}
