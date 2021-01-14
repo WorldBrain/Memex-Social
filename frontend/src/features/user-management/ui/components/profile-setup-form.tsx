f25dded1dc17f72880f88912b4ef639426f0480a
Charris <c@harrischris.com>
2021-01-14 16:21:29 +0100

    Potential fix to input selection via stateful component

--- frontend/src/features/user-management/ui/components/profile-setup-form.tsx
+++ frontend/src/features/user-management/ui/components/profile-setup-form.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {ChangeEvent} from "react";
 import styled from "styled-components";
 import TextInput from "../../../../common-ui/components/text-input";
 import Button from "../../../../common-ui/components/button";
@@ -16,50 +16,73 @@ const DisplayName = styled.div`
   text-align: center;
 `;
 
-export default function ProfileSetupForm(props: {
+interface Props {
   displayName: string;
   onDisplayNameChange(value: string): void;
   onConfirm(): void;
-}) {
-  const placeholder = "John Doe";
-  return (
-    <StyledProfileSetupForm>
-      <Margin bottom="medium">
-        <Header>Set up your display name</Header>
-      </Margin>
-      <Margin bottom="medium">
-        <DisplayName>
-          <TextInput
-            placeholder={placeholder}
-            value={props.displayName}
-            onChange={(e) => props.onDisplayNameChange(e.target.value)}
-            onConfirm={props.onConfirm}
-          />
-        </DisplayName>
-      </Margin>
-      <Margin bottom="small">
-        <Header>Example</Header>
-      </Margin>
-      <Margin bottom="small">
-        <AnnotationReply
-          user={{
-            displayName:
-              props.displayName?.length > 0 ? props.displayName : placeholder,
-          }}
-          reply={{
-            content:
-              "This is what a reply to someone's note looks like with your name attached",
-            createdWhen: Date.now(),
-            normalizedPageUrl: "something",
-          }}
-        />
-      </Margin>
-      <Button
-        type="primary-action"
-        onClick={props.displayName.length ? props.onConfirm : undefined}
-      >
-        Confirm
-      </Button>
-    </StyledProfileSetupForm>
-  );
+}
+
+interface State {
+  displayName: string;
+}
+
+export default class ProfileSetupForm extends React.Component<Props,State> {
+
+    state = {displayName: ""}
+
+    constructor(props:Props) {
+        super(props);
+        this.state.displayName = props.displayName;
+
+    }
+
+    onDisplayNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+        this.setState({displayName:e.target.value})
+        this.props.onDisplayNameChange(e.target.value)
+    }
+
+  render() {
+    const placeholder = "John Doe";
+
+    return (
+        <StyledProfileSetupForm>
+          <Margin bottom="medium">
+            <Header>Set up your display name</Header>
+          </Margin>
+          <Margin bottom="medium">
+            <DisplayName>
+              <TextInput
+                  placeholder={placeholder}
+                  value={this.state.displayName}
+                  onChange={(e) => this.onDisplayNameChange(e)}
+                  onConfirm={this.props.onConfirm}
+              />
+            </DisplayName>
+          </Margin>
+          <Margin bottom="small">
+            <Header>Example</Header>
+          </Margin>
+          <Margin bottom="small">
+            <AnnotationReply
+                user={{
+                  displayName:
+                      this.props.displayName?.length > 0 ? this.props.displayName : placeholder,
+                }}
+                reply={{
+                  content:
+                      "This is what a reply to someone's note looks like with your name attached",
+                  createdWhen: Date.now(),
+                  normalizedPageUrl: "something",
+                }}
+            />
+          </Margin>
+          <Button
+              type="primary-action"
+              onClick={this.props.displayName.length ? this.props.onConfirm : undefined}
+          >
+            Confirm
+          </Button>
+        </StyledProfileSetupForm>
+    );
+  }
 }
