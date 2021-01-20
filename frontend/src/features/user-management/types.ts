bd512827c5e68dda41d3a108de280f04298ececa
Sean Coley <seanwcoley@gmail.com>
2021-01-20 17:04:47 +0100

    Update user public profile storage naming and types

--- frontend/src/features/user-management/types.ts
+++ frontend/src/features/user-management/types.ts
@@ -1,2 +1,17 @@
-import { UserPublicProfile } from "@worldbrain/memex-common/ts/web-interface/types/storex-generated/user-management";
-export default UserPublicProfile;
+import { UserReference as UserRef } from '@worldbrain/memex-common/lib/web-interface/types/users'
+import {
+    UserPublicProfile as Profile,
+    User as ImportedUser,
+} from '@worldbrain/memex-common/ts/web-interface/types/storex-generated/user-management'
+
+export type UserPublicProfile = Profile
+export type User = ImportedUser
+export type UserReference = UserRef
+export interface ProfileWebLink {
+    url: string
+    iconPath:
+        | 'img/websiteIcon.svg'
+        | 'img/mediumIcon.svg'
+        | 'img/twitterIcon.svg'
+        | 'img/substackIcon.svg'
+}
