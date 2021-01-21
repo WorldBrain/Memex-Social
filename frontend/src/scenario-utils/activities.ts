0f8b771e12936b53cd6dc78bb478e0b02d5e5c5a
Vincent den Boer <vincent@youapt.eu>
2021-01-21 16:41:58 +0100

    sharedList stream activities and scenario

--- frontend/src/scenario-utils/activities.ts
+++ frontend/src/scenario-utils/activities.ts
@@ -35,6 +35,11 @@ export const setupTestActivities = async ({ services, storage }: { services: Ser
         entity: { type: 'shared-annotation-reference', id: 'third-annotation' },
         feeds: { home: true },
     })
+    await storage.serverModules.activityFollows.storeFollow({
+        userReference: services.auth.getCurrentUserReference()!,
+        collection: 'sharedList',
+        objectId: 'default-list'
+    })
     await services.auth.logout()
 
     await services.auth.loginWithEmailPassword({
@@ -77,6 +82,14 @@ export const setupTestActivities = async ({ services, storage }: { services: Ser
         normalizedPageUrl: 'notion.so',
         isFirstReply: false,
         reply: { content: 'Test reply four' }
+    })
+    await storage.serverModules.contentSharing.createListEntries({
+        listReference: { type: 'shared-list-reference', id: 'default-list' },
+        listEntries: [
+            { createdWhen: Date.now(), entryTitle: 'New.com - page one', normalizedUrl: 'new.com/one', originalUrl: 'https://www.new.com/one' },
+            { createdWhen: Date.now(), entryTitle: 'New.com - page two', normalizedUrl: 'new.com/two', originalUrl: 'https://www.new.com/two' },
+        ],
+        userReference: services.auth.getCurrentUserReference()!
     })
     await services.auth.logout()
 
