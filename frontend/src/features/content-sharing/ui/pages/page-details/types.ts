299a9e0d8dc4a2a77c7a580197d77ae2e1b0b6ba
Sean Coley <seanwcoley@gmail.com>
2021-01-26 13:56:32 +0100

    Formatting

--- frontend/src/features/content-sharing/ui/pages/page-details/types.ts
+++ frontend/src/features/content-sharing/ui/pages/page-details/types.ts
@@ -1,22 +1,48 @@
-import { SharedAnnotationReference, SharedAnnotation, SharedPageInfoReference, SharedPageInfo } from "@worldbrain/memex-common/lib/content-sharing/types";
-import UserStorage from "../../../../user-management/storage";
-import { UIEvent, UISignal } from "../../../../../main-ui/classes/logic";
-import { UIElementServices } from "../../../../../main-ui/classes";
-import { AnnotationConversationEvent, AnnotationConversationsState, AnnotationConversationSignal } from "../../../../content-conversations/ui/types";
-import { UITaskState } from "../../../../../main-ui/types";
-import { User, UserReference } from "@worldbrain/memex-common/lib/web-interface/types/users";
-import { StorageModules } from "../../../../../storage/types";
+import {
+    SharedAnnotationReference,
+    SharedAnnotation,
+    SharedPageInfoReference,
+    SharedPageInfo,
+} from '@worldbrain/memex-common/lib/content-sharing/types'
+import UserStorage from '../../../../user-management/storage'
+import { UIEvent, UISignal } from '../../../../../main-ui/classes/logic'
+import { UIElementServices } from '../../../../../main-ui/classes'
+import {
+    AnnotationConversationEvent,
+    AnnotationConversationsState,
+    AnnotationConversationSignal,
+} from '../../../../content-conversations/ui/types'
+import { UITaskState } from '../../../../../main-ui/types'
+import {
+    User,
+    UserReference,
+} from '@worldbrain/memex-common/lib/web-interface/types/users'
+import { StorageModules } from '../../../../../storage/types'
 
 export interface PageDetailsDependencies {
-    services: UIElementServices<'contentConversations' | 'auth' | 'overlay' | 'activityStreams' | 'router'>;
-    storage: Pick<StorageModules, 'contentSharing' | 'contentConversations' | 'users' | 'activityStreams'>
+    services: UIElementServices<
+        | 'contentConversations'
+        | 'auth'
+        | 'overlay'
+        | 'activityStreams'
+        | 'router'
+    >
+    storage: Pick<
+        StorageModules,
+        'contentSharing' | 'contentConversations' | 'users' | 'activityStreams'
+    >
     pageID: string
     userManagement: UserStorage
 }
 
 export type PageDetailsState = AnnotationConversationsState & {
     annotationLoadState: UITaskState
-    annotations?: Array<SharedAnnotation & { reference: SharedAnnotationReference, linkId: string }> | null
+    annotations?: Array<
+        SharedAnnotation & {
+            reference: SharedAnnotationReference
+            linkId: string
+        }
+    > | null
 
     pageInfoLoadState: UITaskState
     pageInfoReference?: SharedPageInfoReference | null
@@ -30,6 +56,5 @@ export type PageDetailsState = AnnotationConversationsState & {
 export type PageDetailsEvent = UIEvent<AnnotationConversationEvent>
 
 export type PageDetailsSignal = UISignal<
-    { type: 'loaded' } |
-    AnnotationConversationSignal
+    { type: 'loaded' } | AnnotationConversationSignal
 >
