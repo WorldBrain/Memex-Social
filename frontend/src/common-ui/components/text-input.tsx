029f210e34bbe290a2aef31f08dd9a9bfe8043b8
Sean Coley <seanwcoley@gmail.com>
2021-01-28 14:47:06 +0100

    Refactor text input to make padding optional

--- frontend/src/common-ui/components/text-input.tsx
+++ frontend/src/common-ui/components/text-input.tsx
@@ -2,29 +2,65 @@ import React from "react";
 import { InputHTMLAttributes } from "react";
 import styled from "styled-components";
 
-const StyledInput = styled.input`
+import { theme } from '../../main-ui/styles/theme'
+import { Theme } from '../../main-ui/styles/types'
+
+const StyledInput = styled.input<{
+  theme: Theme
+  padding?: boolean
+}>`
   font-family: ${(props) => props.theme.fonts.primary};
   background: ${(props) => props.theme.colors.grey};
   border: 0;
-  border-radius: 3px;
-  padding: 10px;
-`;
+  border-radius: ${props => props.theme.borderRadii.default};
+  ${props => props.padding ? 'padding: 10px;' : '' // hacky workaround as this component is already used in several places
+}; 
+`
+
+export const StyledInputLabel = styled.div<{
+  theme: Theme
+}>`
+  font-family: ${(props) => props.theme.fonts.primary};
+  font-weight: ${(props) => props.theme.fontWeights.bold};
+  font-size: ${(props) => props.theme.fontSizes.text};
+  line-height: ${(props) => props.theme.lineHeights.text};
+  color: ${props => props.theme.colors.primary};
+`
 
 export default function TextInput(
   props: InputHTMLAttributes<HTMLInputElement> & {
-    onConfirm?(): void;
+    onConfirm?(): void
+    label?: string
   }
 ) {
-  return (
-    <StyledInput
-      type="text"
-      {...props}
-      onKeyDown={(event) => {
-        if (props.onConfirm && event.keyCode === 13) {
-          return props.onConfirm();
-        }
-        props.onKeyDown?.(event);
-      }}
-    />
-  );
+  const renderElement = function(padding?: boolean) {
+    return (
+        <StyledInput
+        padding={padding}
+        type="text"
+        theme={theme}
+        {...props}
+        onKeyDown={(event) => {
+          if (props.onConfirm && event.keyCode === 13) {
+            return props.onConfirm();
+          }
+          props.onKeyDown?.(event);
+        }}
+      />
+    )
+  }
+  if(!props.label) {
+    return (
+      renderElement(true)
+      )
+  } else {
+    return (
+      <>
+        <StyledInputLabel>
+          {props.label}
+        </StyledInputLabel>
+        {renderElement()}
+      </>
+    )
+  }
 }
