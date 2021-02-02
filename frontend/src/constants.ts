85f26583445977628cf739b8fdba5cb71360b6fb
Sean Coley <seanwcoley@gmail.com>
2021-02-02 20:03:47 +0100

    Add additional fields to form

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,5 +1,3 @@
-export default {
-    userPublicProfile: {
-        bioCharLength: 280
-    }
-}
+export const USER_PROFILE_BIO_CHAR_LIMIT = 280
+
+export const DOMAIN_TLD_PATTERN = /^-?(site:)?(\w+\.)?[\w-]{2,}\.\w{2,3}(\.\w{2})?$/
--- frontend/src/features/user-management/ui/containers/profile-edit-modal/index.tsx
+++ frontend/src/features/user-management/ui/containers/profile-edit-modal/index.tsx
@@ -15,6 +15,9 @@ import { theme } from '../../../../../main-ui/styles/theme'
 import ProfileEditModalLogic from './logic'
 import { PrimaryActionButton, SecondaryActionButton } from '../../components/ActionButtons';
 import TextInput from '../../../../../common-ui/components/text-input'
+import { DOMAIN_TLD_PATTERN } from '../../../../../constants';
+import TextArea from '../../../../../common-ui/components/text-area';
+const cameraImage = require('../../../../../assets/img/cameraIcon.svg')
 
 const Container = styled.div<{ theme: Theme }>`
     min-height: 358px;
@@ -58,6 +61,33 @@ const FormColumn = styled.div<{
     ${props => props.maxWidth && `max-width: ${props.maxWidth}`};
 `
 
+const LargeUserAvatar = styled.div<{ path: string }>`
+    height: 100px;
+    width: 100px;
+    border-radius: 50px;
+    background-position: center;
+    background-size: contain;
+    background: url(${props => props.path});
+`
+
+const AvatarPlaceholder = styled.div<{ theme: Theme }>`
+    height: 100px;
+    width: 100px;
+    border-radius: 50px;
+    background: ${props => props.theme.colors.grey};
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+const CameraIcon = styled.div`
+    height: 30px;
+    width: 30px;
+    background-position: center;
+    background-size: contain;
+    background-image: url(${cameraImage});
+`
+
 export type ProfilePopupProps = ProfileEditModalDependencies
 
 export default class ProfileEditModal extends UIElement<
@@ -73,14 +103,16 @@ export default class ProfileEditModal extends UIElement<
     private urlInputErrorMessage: string = 'This must be a valid URL'
 
     handleSaveClick() {
-        this.processEvent('saveUserPublicProfile', { profileData: this.state.profileData,  })
+        this.processEvent('saveUserPublicProfile', { profileData: this.state.profileData, displayName: this.state.user?.displayName ?? '' })
     }
 
     handleCancelClick() {
         this.processEvent('hidePopup', null)
     }
 
-    handleSetDisplayName
+    handleSetDisplayName(evt: React.ChangeEvent<HTMLInputElement>) {
+        this.processEvent('setDisplayName', { value: evt.currentTarget.value })
+    }
 
     handleSetProfileValue(key: keyof UserPublicProfile, value: string): void {
         this.processEvent('setProfileValue', { key, value })
@@ -122,11 +154,52 @@ export default class ProfileEditModal extends UIElement<
                                 Profile Information
                             </SectionHeader>
                         </Margin>
-                        <FormColumn maxWidth="260px">
+                        <FormColumn maxWidth="263px">
                             <TextInput 
                                 label="Display Name"
                                 value={this.state.user?.displayName}
+                                onConfirm={() => this.testDisplayName()}
+                                error={this.state.inputErrorArray[0]}
+                                errorMessage={this.displayNameErrorMessage}
                             />
+                            <TextArea
+                                label="Bio"
+                                value={this.state.profileData.bio}
+                            />
+                            <TextInput
+                                label="Website"
+                                value={this.state.profileData.websiteURL}
+                                onConfirm={() => this.testValidURL(1, this.state.profileData.websiteURL)}
+                                error={this.state.inputErrorArray[1]}
+                                errorMessage={this.urlInputErrorMessage}
+                            />
+                            <TextInput
+                                label="Twitter"
+                                value={this.state.profileData.twitterURL}
+                                onConfirm={() => this.testValidURL(2, this.state.profileData.twitterURL)}
+                                error={this.state.inputErrorArray[2]}
+                                errorMessage={this.urlInputErrorMessage}
+                            />
+                            <TextInput
+                                label="Medium"
+                                value={this.state.profileData.mediumURL}
+                                onConfirm={() => this.testValidURL(3, this.state.profileData.mediumURL)}
+                                error={this.state.inputErrorArray[3]}
+                                errorMessage={this.urlInputErrorMessage}
+                            />
+                            <TextInput
+                                label="Substack"
+                                value={this.state.profileData.substackURL}
+                                onConfirm={() => this.testValidURL(4, this.state.profileData.substackURL)}
+                                error={this.state.inputErrorArray[4]}
+                                errorMessage={this.urlInputErrorMessage}
+                            />
+                        </FormColumn>
+                        <FormColumn maxWidth="186px">
+                            {this.state.profileData.avatarURL && <LargeUserAvatar path={this.state.profileData.avatarURL} />}
+                            {!this.state.profileData.avatarURL && <AvatarPlaceholder>
+                                <CameraIcon />
+                            </AvatarPlaceholder>}
                         </FormColumn>
                     </FormContainer>
                 </Padding>
