49a02855e1d8a4060dad1d932ed1e27b2afe2406
Sean Coley <seanwcoley@gmail.com>
2021-02-02 20:02:29 +0100

    Add validation errors to text input

--- frontend/src/common-ui/components/text-input.tsx
+++ frontend/src/common-ui/components/text-input.tsx
@@ -8,13 +8,14 @@ import { Theme } from '../../main-ui/styles/types'
 const StyledInput = styled.input<{
   theme: Theme
   padding?: boolean
+  error?: boolean
 }>`
   font-family: ${(props) => props.theme.fonts.primary};
   background: ${(props) => props.theme.colors.grey};
   border: 0;
   border-radius: ${props => props.theme.borderRadii.default};
-  ${props => props.padding ? 'padding: 10px;' : '' // hacky workaround as this component is already used in several places
-}; 
+  ${props => props.padding ? 'padding: 10px;' : ''}
+  ${props => props.error && 'border: solid 2px red;'}
 `
 
 export const StyledInputLabel = styled.div<{
@@ -27,19 +28,32 @@ export const StyledInputLabel = styled.div<{
   color: ${props => props.theme.colors.primary};
 `
 
+export const ErrorMessage = styled.div<{
+  theme: Theme
+}>`
+  font-family: ${props => props.theme.fonts.primary};
+  font-size: ${props => props.theme.fontSizes.smallText};
+  line-height: ${props => props.theme.lineHeights.smallText};
+  color: ${props => props.theme.colors.warning};
+`
+
 export default function TextInput(
   props: InputHTMLAttributes<HTMLInputElement> & {
     onConfirm?(): void
     label?: string
+    error?: boolean
+    errorMessage?: string
   }
 ) {
   const renderElement = function(padding?: boolean) {
     return (
+      <>
         <StyledInput
         padding={padding}
         type="text"
         theme={theme}
         {...props}
+        error={props.error}
         onKeyDown={(event) => {
           if (props.onConfirm && event.keyCode === 13) {
             return props.onConfirm();
@@ -47,6 +61,8 @@ export default function TextInput(
           props.onKeyDown?.(event);
         }}
       />
+      {props.error && props.errorMessage && <ErrorMessage theme={theme}>{props.errorMessage}</ErrorMessage>}
+    </>
     )
   }
   if(!props.label) {
