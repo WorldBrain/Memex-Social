1ef886a80027b40fc0e20e845c0776613195d780
Sean Coley <seanwcoley@gmail.com>
2021-02-08 20:37:47 +0100

    Change URL test regex

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,3 +1,3 @@
 export const USER_PROFILE_BIO_CHAR_LIMIT = 280
 
-export const DOMAIN_TLD_PATTERN = /^-?(site:)?(\w+\.)?[\w-]{2,}\.\w{2,3}(\.\w{2})?$/
+export const VALID_URL_TEST = /^(ftp|http|https):\/\/[^ "]+$/
