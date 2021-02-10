dc616fa6dddfd655bdec9fd1e68159191b7b433f
Sean Coley <seanwcoley@gmail.com>
2021-02-10 17:48:59 +0100

    Complete web monetization icon

--- frontend/src/features/web-monetization/ui/components/web-monetization-icon/index.tsx
+++ frontend/src/features/web-monetization/ui/components/web-monetization-icon/index.tsx
@@ -1,11 +1,14 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Icon from '../../../../../common-ui/components/icon'
 import { UIElement } from '../../../../../main-ui/classes'
+import Logic from './logic'
 import {
     WebMonetizationButtonDependencies,
     WebMonetizationButtonState,
     WebMonetizationButtonEvent,
 } from '../../../logic/buttons/types'
+import CuratorSupportPopupContainer from '../../../../user-management/ui/containers/curator-support-popup-container'
+import LoadingScreen from '../../../../../common-ui/components/loading-screen'
 
 type WebMonetizationIconDependencies = WebMonetizationButtonDependencies
 type WebMonetizationIconState = WebMonetizationButtonState
@@ -16,19 +19,37 @@ export default class WebMonetizationIcon extends UIElement<
     WebMonetizationIconState,
     WebMonetizationIconEvent
 > {
+    constructor(props: WebMonetizationIconDependencies) {
+        super(props, { logic: new Logic(props) })
+    }
+
     handleClick: React.MouseEventHandler = () => {
         this.processEvent('makeSupporterPayment', null)
     }
 
     render() {
         return (
-            <Icon
-                onClick={this.handleClick}
-                height="20px"
-                fileName={`web-monetization-logo${
-                    this.state.paymentMade ? '-confirmed' : ''
-                }.svg`}
-            />
+            <>
+            {this.state.isDisplayed && this.state.initialLoadTaskState === 'running' && (
+                    <LoadingScreen />
+                )}
+            {this.state.isDisplayed && this.state.initialLoadTaskState === 'success' && (
+                <CuratorSupportPopupContainer 
+                    services={this.props.services}
+                    storage={this.props.storage}
+                    userRef={this.props.userRef}
+                    >
+                    <Icon
+                        onClick={this.handleClick}
+                        height="20px"
+                        fileName={`web-monetization-logo${
+                            this.state.paymentMade ? '-confirmed' : ''
+                        }.svg`}
+                        />
+                </CuratorSupportPopupContainer>
+            )}
+                
+            </>
         )
     }
 }
