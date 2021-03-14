ce7b9dca329ea5128adc4aab9d87a43ea69289d3
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2021-03-14 14:19:20 +0700

    Update item-box related comps to use memex-common versions
    - this is broken as the theme doesn't get passed down properly

--- frontend/src/common-ui/components/item-box.tsx
+++ frontend/src/common-ui/components/item-box.tsx
@@ -1,23 +1,3 @@
-import React from 'react'
-import styled from 'styled-components'
+import ItemBox from '@worldbrain/memex-common/lib/common-ui/components/item-box'
 
-export type ItemBoxVariant = 'new-item'
-
-const StyledItemBox = styled.div<{ variant?: ItemBoxVariant }>`
-    font-family: ${(props) => props.theme.fonts.primary};
-    background: #ffffff;
-    box-sizing: border-box;
-    box-shadow: rgba(0, 0, 0, 0.1) 0px 1px 2px 0px;
-    border-radius: 5px;
-    text-decoration: none;
-    width: 100%;
-`
-
-export default function ItemBox(props: {
-    children: React.ReactNode
-    variant?: ItemBoxVariant
-}) {
-    return (
-        <StyledItemBox variant={props.variant}>{props.children}</StyledItemBox>
-    )
-}
+export default ItemBox
