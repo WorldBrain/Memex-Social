7aef084c2951a62aa86763064f00edbd325584a7
Vincent den Boer <vincent@youapt.eu>
2021-03-18 12:14:46 +0100

    Fix item box and creation info regressions

--- frontend/src/common-ui/components/item-box-bottom.tsx
+++ frontend/src/common-ui/components/item-box-bottom.tsx
@@ -1,3 +1,31 @@
-import ItemBoxBottom from '@worldbrain/memex-common/lib/common-ui/components/item-box-bottom'
+import React from 'react'
+import CommonItemBoxBottom, {
+    ItemBoxBottomProps,
+} from '@worldbrain/memex-common/lib/common-ui/components/item-box-bottom'
+import ProfilePopupContainer, {
+    ProfilePopupProps,
+} from '../../features/user-management/ui/containers/profile-popup-container'
 
-export default ItemBoxBottom
+export default function ItemBoxBottom(
+    props: ItemBoxBottomProps & { profilePopupProps?: ProfilePopupProps },
+) {
+    const renderWithPopup: ItemBoxBottomProps['renderCreationInfo'] = ({
+        children,
+    }) => (
+        <ProfilePopupContainer {...props.profilePopupProps!}>
+            {children}
+        </ProfilePopupContainer>
+    )
+    const renderWithoutPopup: ItemBoxBottomProps['renderCreationInfo'] = ({
+        children,
+    }) => children
+
+    return (
+        <CommonItemBoxBottom
+            {...props}
+            renderCreationInfo={
+                props.profilePopupProps ? renderWithPopup : renderWithoutPopup
+            }
+        />
+    )
+}
