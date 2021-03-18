c603bfee2a0f0fbadf03ad2083faaaad457db9fd
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2021-03-18 11:29:04 +0700

    Revert "Move various UI classes and types to memex-common"
    This reverts commit 87da362b460e58b6a5decd7055b44b706d4abad1.

--- frontend/src/main-ui/types/index.ts
+++ frontend/src/main-ui/types/index.ts
@@ -2,7 +2,7 @@ import type * as history from 'history'
 import { Services } from '../../services/types'
 import { Storage } from '../../storage/types'
 
-export type { UITaskState } from '@worldbrain/memex-common/lib/common-ui/types'
+export type UITaskState = 'pristine' | 'running' | 'success' | 'error'
 
 export type UiRunner = (options: {
     services: Services
