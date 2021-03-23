3c41878a01a8c032d43e27ac6dd6a97b3892241a
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2021-03-23 16:57:25 +0700

    Move easy common-ui components to memex-social

--- frontend/src/common-ui/components/select.tsx
+++ frontend/src/common-ui/components/select.tsx
@@ -1,35 +1,5 @@
-import React from 'react'
-import styled from 'styled-components'
-
-export interface OptionProps<T> {
-    value: T
-    subText: string
-    headerText: string
-}
-
-export interface SelectProps<T> {
-    value: T
-    options: OptionProps<T>[]
-    onChange: (nextValue: T) => void
-}
-
-export default function Select<ValueType extends string | number>(
-    props: SelectProps<ValueType>,
-) {
-    const handleValueChange: React.ChangeEventHandler<HTMLSelectElement> = (
-        e,
-    ) => props.onChange(e.target.value as ValueType)
-
-    return (
-        <StyledSelect value={props.value} onChange={handleValueChange}>
-            {props.options.map(({ value, headerText, subText }) => (
-                <StyledOption key={value} value={value}>
-                    {headerText}
-                </StyledOption>
-            ))}
-        </StyledSelect>
-    )
-}
-
-const StyledSelect = styled.select``
-const StyledOption = styled.option``
+export { default } from '@worldbrain/memex-common/lib/common-ui/components/select'
+export type {
+    OptionProps,
+    SelectProps,
+} from '@worldbrain/memex-common/lib/common-ui/components/select'
