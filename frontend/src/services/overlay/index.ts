6c9e7c19852a068a78ffa27922ad9cfdee844393
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2021-03-26 12:43:21 +0700

    Move logic-registry+overlay services to memex-common
    - these implementations are completely shareable - for now

--- frontend/src/services/overlay/index.ts
+++ frontend/src/services/overlay/index.ts
@@ -1,7 +1,1 @@
-import { EventEmitter } from 'events'
-
-import type { OverlayServiceInterface } from '@worldbrain/memex-common/lib/services/overlay/types'
-
-export default class OverlayService implements OverlayServiceInterface {
-    events = new EventEmitter()
-}
+export { default } from '@worldbrain/memex-common/lib/services/overlay'
