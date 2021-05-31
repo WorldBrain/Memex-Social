cde5c50c7755cace3d842ed627426063e9ac7db8
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2021-05-31 14:32:54 +0700

    Move new-reply + deps to memex-common

--- frontend/src/common-ui/utils/dom-events.tsx
+++ frontend/src/common-ui/utils/dom-events.tsx
@@ -1,13 +1,1 @@
-import React from 'react'
-
-export function keyPressAction(e: React.KeyboardEvent<HTMLElement>) {
-    if (e.which === 13 || e.keyCode === 13 || e.key === 'Enter') {
-        if (e.ctrlKey || e.metaKey) {
-            return 'confirm'
-        }
-    }
-    if (e.key === 'Escape') {
-        return 'cancel'
-    }
-    return null
-}
+export * from '@worldbrain/memex-common/lib/common-ui/utils/dom-events'
