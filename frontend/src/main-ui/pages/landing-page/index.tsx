ea3c060bc00d0d0fc83d9d6239eb99c5752097af
Oliver <oli@worldbrain.io>
2021-11-06 00:02:18 +0000

    make solid loadingindicator for the page to look nice and professional

--- frontend/src/main-ui/pages/landing-page/index.tsx
+++ frontend/src/main-ui/pages/landing-page/index.tsx
@@ -1,14 +1,23 @@
 import { Trans } from 'react-i18next'
 import React from 'react'
-// import styled from "styled-components";
+import styled from 'styled-components'
 import { UIElement } from '../../classes'
 import { UIElementServices } from '../../../services/types'
 import Logic, { LandingPageState } from './logic'
 import { LandingPageEvent } from './types'
+import LoadingIndicator from '../../../common-ui/components/loading-indicator'
 
 interface LandingPageProps {
     services: UIElementServices<'auth' | 'router'>
 }
+
+const Container = styled.div`
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
 
 export default class LandingPage extends UIElement<
     LandingPageProps,
@@ -24,6 +33,12 @@ export default class LandingPage extends UIElement<
     }
 
     render() {
-        return <Trans>Redirecting...</Trans>
+        return (
+            <Trans>
+                <Container>
+                    <LoadingIndicator />
+                </Container>
+            </Trans>
+        )
     }
 }
