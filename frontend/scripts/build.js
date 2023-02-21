a8ac722dccbefbe01be28d1e7540385cce027910
Oliver <oli@worldbrain.io>
2023-02-21 09:46:58 +0100

    Fix used default extensionID

--- frontend/scripts/build.js
+++ frontend/scripts/build.js
@@ -3,7 +3,7 @@
 // Do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = 'production'
 process.env.NODE_ENV = 'production'
-process.env.MEMEX_EXTENSION_ID = 'ielfhekmfbpebfogdeipegfeooelelge'
+process.env.MEMEX_EXTENSION_ID = 'abkfbakhjpmblaafnpgjppbmioombali'
 // staging Oliver: 'ielfhekmfbpebfogdeipegfeooelelge',
 // prod extensionID: 'abkfbakhjpmblaafnpgjppbmioombali',
 
