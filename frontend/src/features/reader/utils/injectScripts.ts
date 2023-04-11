ae93e3413fc0d760b1d732ec954092b66ffb88fd
Bojan Ivanac <bojan@veed.io>
2023-04-11 10:44:13 +0200

    Attach hilightdomrange to iframe

--- frontend/src/features/reader/utils/injectScripts.ts
+++ frontend/src/features/reader/utils/injectScripts.ts
@@ -1,10 +1,14 @@
+import { highlightDOMRange } from '@worldbrain/memex-common/lib/annotations/highlight-dom-range'
+
 const sendMessageFromIframe = (message: string) => {
     console.log(message)
 }
 
 export const attachToWindow = () => {
+    console.log(highlightDOMRange)
     // @ts-ignore
     window.injected = {
         sendMessageFromIframe,
+        highlightDOMRange,
     }
 }
