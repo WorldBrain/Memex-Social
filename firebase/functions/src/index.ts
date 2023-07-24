d0d26c9a47f9a61ab595c9cec447ce1dcd50209a
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2023-07-24 11:43:17 +0700

    Bring Memex-Social's FB function setup up-to-date
    - Basically mirroring the FB backend set up now.
    - Doing this as we're going to want to deploy the annotation social preview function from here with the rest of the hosting setup

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,5 +1,25 @@
 import * as admin from 'firebase-admin'
-import * as functions from 'firebase-functions'
+import * as functionsV1 from 'firebase-functions/v1'
+import { onRequest, onCall } from 'firebase-functions/v2/https'
+import { defineSecret, defineString } from 'firebase-functions/params'
+import {
+    onDocumentCreated,
+    onDocumentDeleted,
+} from 'firebase-functions/v2/firestore'
 import { main } from '@worldbrain/memex-common/lib/firebase-backend/main'
+import type {
+    RequiredFunctionParamsModules,
+    RequiredFunctionV2Modules,
+} from '@worldbrain/memex-common/lib/firebase-backend/types'
 
-module.exports = main(admin as any, functions)
+const functionsV2: RequiredFunctionV2Modules = {
+    https: { onCall, onRequest },
+    firestore: { onDocumentCreated, onDocumentDeleted },
+}
+
+const functionParams: RequiredFunctionParamsModules = {
+    defineSecret,
+    defineString,
+}
+
+module.exports = main(admin, functionsV1, functionsV2, functionParams)
