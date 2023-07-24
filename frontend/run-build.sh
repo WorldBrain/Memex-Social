fcf9efe163e27598b426ec7d61c1cf05ddbef4a3
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2023-07-24 12:55:59 +0700

    Write script to set Web UI JS bundle names for functions to access

---
+++ frontend/run-build.sh
@@ -0,0 +1,25 @@
+# Do main build then move output to hosting dir
+env-cmd -f $ENV_FILE webpack --config webpack/webpack.prod.js
+mkdir -p ../firebase/public
+rm -rf ../firebase/public/*
+mv build/* ../firebase/public/
+
+# Derive bundle names (contain dynamic cache-busting hashes)
+bundle_names=''
+for bundle in ../firebase/public/*.js; do
+    base_name=$(basename $bundle)
+    bundle_names+="${base_name} "
+done
+
+# Remove trailing space
+bundle_names=$(echo $bundle_names | sed 's/ *$//')
+
+# Write latest bundle names to .env.*, so functions can access them
+for env_file in ../firebase/functions/.env*; do
+    # If var already there, replace, else append
+    if grep -q "WEB_UI_JS_BUNDLES=" $env_file; then
+        sed -i '' -e "s/WEB_UI_JS_BUNDLES=.*/WEB_UI_JS_BUNDLES=\"${bundle_names}\"/" $env_file
+    else
+        echo "WEB_UI_JS_BUNDLES=\"${bundle_names}\"" >> $env_file
+    fi
+done
