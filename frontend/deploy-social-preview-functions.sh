06005e45e988554f734ba823aa0088832918db21
Lam Huc Minh <lamhucminh@arkady.local>
2023-09-21 14:12:18 +0700

    Write script to deploy social preview fns before hosting
    - Assumes the existence and setup of the firebase-backend repo on the same level as the Memex-Social repo.
    - Mainly meant to solve a deployment pain we currently have where we have to manually do all this every deploy else things don't work

---
+++ frontend/deploy-social-preview-functions.sh
@@ -0,0 +1,34 @@
+FB_REPO_DIR=../../firebase-backend
+
+if ! test -d $FB_REPO_DIR; then
+    echo 'firebase-backend repo not found as a sibling dir to Memex-Social repo. Please clone it'
+    exit 1
+fi
+
+if [ "$1" != "production" ] && [ "$1" != "staging" ]; then
+    echo 'First arg must be either "production" or "staging"'
+    exit 1
+fi
+
+# Copy over generated .env.{staging,production} file from Memex-Social's firebase/functions dir to firebase-backend repo's
+cp ../firebase/functions/.env.$1 $FB_REPO_DIR/firebase/functions/
+
+# Ensure firebase-backend repo's memex-common's HEAD is pointing to the same commit as Memex-Social's
+MEMEX_COMMON_HEAD=$(
+    git submodule status ../external/@worldbrain/memex-common |
+    awk '{print $1}' |  # Grab only the commit hash from the entire status
+    sed 's/^+//'        # Remove any leading '+' from the hash
+)
+cd $FB_REPO_DIR/firebase/functions/external/@worldbrain/memex-common
+git checkout $MEMEX_COMMON_HEAD
+
+# Now install deps in firebase/functions
+cd ../../..
+yarn
+
+# Now install deps in repo root
+cd ../..
+yarn
+
+# Now deploy the social preview functions
+yarn firebase -P $1 deploy --only functions:generateAnnotationSocialPreview,functions:generatePageSocialPreview,functions:generateListSocialPreview
